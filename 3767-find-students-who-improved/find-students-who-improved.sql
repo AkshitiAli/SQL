ca826b14debfdf212061601f9fd7d41707da1d21
Akshiti Ali <151648047+AkshitiAli@users.noreply.github.com>
2026-01-29 19:27:40 +0530

    Time: 311 ms (24.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+WITH FirstAndLatest AS (
+    SELECT
+        student_id,
+        subject,
+        MIN(exam_date) AS first_date,
+        MAX(exam_date) AS last_date
+    FROM Scores
+    GROUP BY student_id, subject
+    HAVING COUNT(*) > 1
+)
+SELECT
+    f.student_id,
+    f.subject,
+    s1.score AS first_score,
+    s2.score AS latest_score
+FROM FirstAndLatest f
+JOIN Scores s1
+    ON f.student_id = s1.student_id
+   AND f.subject = s1.subject
+   AND f.first_date = s1.exam_date
+JOIN Scores s2
+    ON f.student_id = s2.student_id
+   AND f.subject = s2.subject
+   AND f.last_date = s2.exam_date
+WHERE s2.score > s1.score
+ORDER BY f.student_id, f.subject;
